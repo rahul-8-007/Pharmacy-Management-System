a9ddd165a54925d579db16b1a2ed2c01ecbbf165
rahul-8-007 <rahulpoluboina18@gmail.com>
2026-04-16 02:39:02 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://pharma-iiit.netlify.app",
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
